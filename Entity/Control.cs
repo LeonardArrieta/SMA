5faae76463fcf03ed0bcbf623c36b301bb35b52f
Leonard Arrieta Cadena <leonard.cadena30@hotmail.com>
2020-05-02 11:03:44 -0500

    Agregar archivos de proyecto.

---
+++ Entity/Control.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    class Control
+    {
+        public double IdControl { get; set; }
+        public string Motivo { get; set; }
+    }
+}
